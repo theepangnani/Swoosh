b2ea28ac99d0e0c580bc912fe5c3b838fc69ef23
theepangnani <theepang@gmail.com>
2019-02-22 00:10:56 -0500

    lession 02

---
+++ app/src/main/java/ca/gnani/swoosh/LeaqueActivity.kt
@@ -0,0 +1,12 @@
+package ca.gnani.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LeaqueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leaque)
+    }
+}
